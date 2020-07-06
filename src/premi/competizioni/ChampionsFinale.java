a8d77a26c4b445df06eb72deb3870cb55a0044c4
Giovanni <gcad96@gmail.com>
2020-07-06 16:19:47 +0200

    update

---
+++ src/premi/competizioni/ChampionsFinale.java
@@ -0,0 +1,12 @@
+package premi.competizioni;
+
+public class ChampionsFinale extends ScontriDiretti
+{
+    public ChampionsFinale()
+    {
+        this.nomeFile = "ChampionsFinale";
+        this.nome = "Champions League";
+        this.creditiVittoria = 40f;
+        this.creditiPareggio = 15f;
+    };
+}
